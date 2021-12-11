4a969fb31dee12b74c1d041cf8fcb7372ae30b38
Howe <yyhdyx98@163.com>
2021-12-11 15:54:08 +0800

    first-01

---
+++ redis-01/src/test/java/com/mystudy/Redis01ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class Redis01ApplicationTests {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    void contextLoads() {
+      
+    }
+
+}
