ce1246ecf61710ace1628bd1716fe3b1cb6342a3
Howe <yyhdyx98@163.com>
2021-12-11 16:01:28 +0800

    first-02

---
+++ redis-01/src/main/java/com/mystudy/controller/TestUtils.java
@@ -0,0 +1,10 @@
+package com.mystudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+@Controller
+public class TestUtils {
+    public static void main(String[] args) {
+        System.out.printf("666");
+    }
+}
